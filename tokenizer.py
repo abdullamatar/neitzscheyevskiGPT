d4f0e8fbb96721fad3d89af8e20fbd447acd0b94
Abdulla <abdullabloushi@gmail.com>
2023-10-08 17:53:42 +0400

    small changes to loss, and num epochs

--- tokenizer.py
+++ tokenizer.py
@@ -1,14 +1,14 @@
 class TextProcessor:
-    def __init__(self, filename: str):
+    def __init__(self, filename: str) -> None:
         with open(filename, "r", encoding="utf-8") as f:
-            self.text = f.read()
-        self.chars = sorted(list(set(self.text)))
-        self.vocab_size = len(self.chars)
-        self.stoi = {c: i for i, c in enumerate(self.chars)}
-        self.itos = {i: c for i, c in enumerate(self.chars)}
+            self.text: str = f.read()
+        self.chars: list[str] = sorted(list(set(self.text)))
+        self.vocab_size: int = len(self.chars)
+        self.stoi: dict = {c: i for i, c in enumerate(self.chars)}
+        self.itos: dict = {i: c for i, c in enumerate(self.chars)}
 
-    def map_to_int(self, text):
+    def map_to_int(self, text: str) -> list[int]:
         return [self.stoi[c] for c in text]
 
-    def decode_mapping(self, integer_representation):
+    def decode_mapping(self, integer_representation: list[int]) -> str:
         return "".join([self.itos[i] for i in integer_representation])
